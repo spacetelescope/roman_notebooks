68ff553879a0aa17bfc7be5423a8416325f43664
Javier Sanchez <jsanchez@stsci.edu>
2026-02-28 14:35:10 -0500

    updated notebook, added additional survey maps

--- notebooks/footprint_visualization/footprint_utils.py
+++ notebooks/footprint_visualization/footprint_utils.py
@@ -8,6 +8,7 @@ import hpgeom as hpg
 import pandas as pd
 import pysiaf
 from pysiaf.utils.rotations import attitude
+import warnings
 rsiaf = pysiaf.Siaf('Roman')
 wfi_cen = rsiaf['WFI_CEN']
 sensors = [rsiaf[f'WFI{j:02d}_FULL'] for j in range(1, 19)]
@@ -179,3 +180,115 @@ def read_pointings_file(filename, reset_index=True):
         return df, df2
     else:
         return df.reset_index(), df2.reset_index()
+
+def get_survey_maps_uri(survey_name, band, tier=None):
+    """
+    Utility function to retrieve the relevant pre-generated
+    survey exposure-time maps
+
+    Params:
+    -------
+    survey_name (str) : Survey name -- allowed options are [HLTDS, HLWAS, GBTDS, GPS]
+    band (str) : Filter element of interest 
+                 -- allowed options are [F062, F087, F106, F129, F146, F158, F814, F213, PRISM, GRISM]
+    tier (str) : Survey tier -- only available for HLWAS and HLTDS
+
+    Returns:
+    --------
+    file_name (str) : Path to pre-generated healsparse map containing the exposure time for the 
+    selected survey / filter combination
+    """
+
+    allowed_surveys = ['HLTDS', 'HLWAS', 'GBTDS', 'GPS']
+    allowed_bands = ['F062', 'F087', 'F106', 'F129', 'F146', 'F158', 'F184', 'F213', 'PRISM', 'GRISM']
+    allowed_hlwas_tier = ['ALL', 'DEEP', 'WIDE', 'MEDIUM', 'ULTRA-DEEP', 'ULTRADEEP']
+    allowed_hltds_tier = ['ALL', 'DEEP', 'WIDE']
+    if survey_name.upper() not in allowed_surveys:
+        raise ValueError('The requested survey is not one of the Community defined surveys')
+
+    if band.upper() not in allowed_bands:
+        raise ValueError('The requested filter element is not available on WFI')
+    # GBTDS logic -- we have all optical elements with their own map but PRISM which is not used
+    if survey_name.upper() == 'GBTDS':
+        if band == 'PRISM':
+            raise ValueError('No PRISM component available for the GBTDS')
+        else:
+            return f'./aux_data/map_{survey_name.upper()}_{band.upper()}.hsp'
+    # GPS logic -- we have all optical elements with their own map
+    elif survey_name.upper() == 'GPS':
+        if band == 'F146':
+            raise ValueError('No F146 component available for the GPS')
+        else:
+            return f'./aux_data/map_{survey_name.upper()}_{band.upper()}.hsp'
+    # HLWAS logic
+    # We have 4 tiers -- wide (only F158), medium (3 filters all the same + grism)
+    # Deep 5 filters with the same exposure time, 1 with different + grism
+    # ultra-deep 3 filters all with the same exposure time we reuse the maps
+    # when possible
+    elif survey_name.upper() == 'HLWAS':
+        if tier is None:
+            warnings.warn('No selected tier for the HLWAS, returning the sum of all tiers')
+            tier = 'ALL'
+        else:
+            if tier.upper() not in allowed_hlwas_tier:
+                raise ValueError('The requested tier name is not available, please try ALL, DEEP, \
+                                  WIDE, MEDIUM, or ULTRA-DEEP')
+            else:
+                if tier.upper() == 'WIDE':
+                    if band.upper() != 'F158':
+                        raise ValueError('HLWAS-WIDE only available for F158')
+                    else:
+                        return f'./aux_data/map_{survey_name.upper()}-{tier.lower()}_{band.upper()}.hsp'
+                elif tier.upper() == 'MEDIUM':
+                    if band.upper() in ['F106', 'F129', 'F158']:
+                        return f'./aux_data/map_{survey_name.upper()}-{tier.lower()}_F158.hsp'
+                    elif band.upper() == 'GRISM':
+                        return f'./aux_data/map_{survey_name.upper()}-{tier.lower()}_GRISM.hsp'
+                    else:
+                        raise ValueError(f'The requested element {band.upper()} is not available \
+                                         for HLWAS-MEDIUM')
+                elif tier.upper() == 'DEEP':
+                    if band.upper() in ['F087', 'F106', 'F129', 'F158', 'F184', 'F213']:
+                        return f'./aux_data/map_{survey_name.upper()}-{tier.lower()}_F213.hsp'
+                    elif band.upper() == 'F146':
+                        return f'./aux_data/map_{survey_name.upper()}-{tier.lower()}_{band.upper()}.hsp'
+                    elif band.upper() == 'GRISM':
+                        return f'./aux_data/map_{survey_name.upper()}-{tier.lower()}_{band.upper()}.hsp'
+                    else:
+                        raise ValueError(f'The requested element {band.upper()} is not available \
+                                          for HLWAS-DEEP')
+                elif (tier.upper() == 'ULTRADEEP') or (tier.upper() == 'ULTRA-DEEP'):
+                    if tier == 'ULTRA-DEEP':
+                        tier = 'ULTRADEEP'
+                    if band.upper() in ['F106', 'F129', 'F158']:
+                        return f'./aux_data/map_{survey_name.upper()}-{tier.lower()}_F158.hsp'
+                    else:
+                        raise ValueError(f'The requested element {band.upper()} is not available \
+                                         for HLWAS-ULTRADEEP')
+                elif tier.upper() == 'ALL':
+                    if band.upper() in ['F087', 'F184', 'F213']:
+                        return f'./aux_data/map_HLWAS-deep_F213.hsp'
+                    elif band.upper() == 'F146':
+                        return f'./aux_data/map_HLWAS-deep_F146.hsp'
+                    elif band.upper() == 'F158':
+                        return f'./aux_data/map_HLWAS-all_F158.hsp'
+                    elif band.upper() == 'GRISM':
+                        return f'./aux_data/map_HLWAS-all_GRISM.hsp'
+                    elif band.upper() in ['F106', 'F129']:
+                        return f'./aux_data/map_HLWAS-all_F106.hsp'
+                    else:
+                        raise ValueError(f'The selected element {band} is not available for HLWAS')
+    
+    elif survey_name.upper() == 'HLTDS':
+        if tier is None:
+            warnings.warn('No selected tier for HLTDS, returning sum of all tiers')
+        else:
+            warnings.warn('Tier separation not implemented currently for HLTDS, returning the sum of all tiers')
+        if band.upper() == 'F184':
+            return f'./aux_data/map_{survey_name.upper()}-deep_{band.upper()}.hsp'
+        elif band.upper() == 'F062':
+            return f'./aux_data/map_{survey_name.upper()}-wide_{band.upper()}.hsp'
+        elif band.upper() in ['PRISM', 'F158', 'F129', 'F106', 'F087']:
+            return f'./aux_data/map_{survey_name.upper()}-deep+wide_{band.upper()}.hsp'
+        else:
+            raise ValueError(f'The requested element {band.upper()} is not available for HLTDS')
